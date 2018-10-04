41d099d94534c1bf5e73e5b7a7382c569a4c50bc
Pavel Logacev <pavel.logacev@gmail.com>
2018-10-04 13:50:27 +0300

    Added assignment 1.

---
+++ assignments/assignment_1/assignment_1.R
@@ -0,0 +1,45 @@
+
+# load the required packages (install them if you don't have them already)
+library(languageR)
+library(tidyverse)
+library(ggplot2)
+
+
+# create a vector named x by assigning it all numbers between 1 and 25
+
+# compute the average of this vector
+
+# determine how many elements the vector has
+
+# convert this integer vector to a character vector
+
+
+# load the 'warlpiri' data.frame from the package 'languageR'
+
+# display the first 3 lines of the data.frame
+
+# look up the meaning of the various columns in the data.frame
+
+# find out how many rows are in the data.frame
+
+# find out which values can occur in the column which indicates the presence of ergative case marking
+
+# determine the overall frequency of ergative case marking in the data set
+
+## In *separate commands*, determine the frequency of ergative case marking as a function of the variables listed
+## below, and please save the results in the specified data frames. Inspect the data frames with View() to make
+## sure that the results makes sense.
+# ... (i) word order (save in data frame called 'proportions_by_WO')
+# ... (ii) overtness of object  (save in data frame called 'proportions_by_ovObject')
+# ... (iii) animacy of the object (save in data frame called 'proportions_by_animObj')
+
+# create plots in ggplot2 (with lines or bars)  from each of the data.frames from the previous step
+
+
+## Respond with a a comment below:
+## Does there seem to be a relation between ergative case marking and ...
+## ... word order
+
+## ... overtness of the object
+
+## ... animacy of the object
